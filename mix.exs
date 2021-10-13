7cf2c5860d297de323a10848a2636f3e5fe75ffe
Antti Halla <antti.halla@gmail.com>
2021-10-13 19:28:25 +0300

    updates and phoenix 1.4 fixes

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ahfi.Mixfile do
 
   def project do
     [app: :ahfi,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
@@ -19,7 +19,7 @@ defmodule Ahfi.Mixfile do
   def application do
     [mod: {Ahfi.Application, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :timex, :comeonin, :earmark ]]
+                    :phoenix_ecto, :postgrex, :timex, :comeonin, :earmark, :swoosh, :gen_smtp, :plug_cowboy ]]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,24 +30,25 @@ defmodule Ahfi.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3"},
+    [{:phoenix, "~> 1.4"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.2"},
      {:bcrypt_elixir, "~> 1.0"},
-     {:plug, "~> 1.3"},
+     {:plug_cowboy, "~> 2.0"},
+     {:plug, "~> 1.7"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     { :earmark, "~> 1.2" },
+  #   { :earmark, "~> 1.2" },
+     {:earmark, git: "https://github.com/ypisetsky/earmark"}, # temp fix, pr pending
      {:timex, "~> 3.1"},
      {:httpoison, "~> 0.13"},
      {:poison, "~> 3.1"},
      {:comeonin, "~> 4.0"},
      {:distillery, "~> 1.0"},
-     {:swoosh, "~> 0.9.1"},
-     {:gen_smtp, "~> 0.12.0"}]
+     {:swoosh, "~> 1.5"},
+     {:gen_smtp, "~> 1.1.0"}]
 
   end
 
